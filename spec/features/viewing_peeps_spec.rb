0ac94c7cc835fd63c615bb46005c2a10818e1439
Ben Partington <bdpartington@gmail.com>
2022-05-02 17:08:08 +0100

    Added test for peep class and implemented

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -3,4 +3,11 @@ feature 'Viewing peeps' do
     visit('/')
     expect(page).to have_content "Chitter"
   end
+
+  scenario 'viewing peeps at /peeps' do
+    visit('/peeps')
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
+  end
 end
